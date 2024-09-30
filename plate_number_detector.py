ae9235ed0c3bf355f2f5ece3fa24950478010294
ASZagam <abdulmaliksz02@gmail.com>
2024-09-30 14:30:42 +0100

    Update plate_number_detector.py

--- plate_number_detector.py
+++ plate_number_detector.py
@@ -8,7 +8,7 @@ import streamlit as st
 
 st.write("PLATE NUMBER DETECTION APP USING YOLO MODEL")
 #load the fine tune model
-model = YOLO("C:/Users/USER/Desktop/object detection/runs/detect/train18/weights/best (1).pt")
+model = YOLO("best (1).pt")
 image_placeholder = st.empty()
 
 
